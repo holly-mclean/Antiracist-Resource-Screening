9e512868f7f94b04613e41b18beaa8db0a60bcbc
Holly McLean <hollym509@gmail.com>
2020-11-14 11:25:32 -0800

    Add project files.

---
+++ Antiracist-Resource-Screening/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Antiracist_Resource_Screening
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
